b371862ad5f0f6483bc4e31642df620349bcc6ee
1 <ppark9553@gmail.com>
2018-06-24 13:10:08 +0900

    corsheaders settings

--- capston/settings.py
+++ capston/settings.py
@@ -54,6 +54,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = False 
+
 ROOT_URLCONF = 'capston.urls'
 
 TEMPLATES = [
